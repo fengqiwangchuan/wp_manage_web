850b430e42787669765868f62203859c02d8c879
王鹏 <wangpeng9812@126.com>
2019-12-19 20:49:54 +0800

    修改图片

--- src/pages/Layout.vue
+++ src/pages/Layout.vue
@@ -6,7 +6,7 @@
       <v-toolbar flat class="transparent">
         <v-list class="pa-0">
           <v-list-tile avatar>
-            <v-list-tile-avatar> <img src="../assets/2.jpeg"> </v-list-tile-avatar>
+            <v-list-tile-avatar> <img src="../assets/2.png"> </v-list-tile-avatar>
             <v-list-tile-content>
               <v-list-tile-title>wp</v-list-tile-title>
             </v-list-tile-content>
