09c925fb53311fb2853678852bfdfd729fb3b714
王鹏 <wangpeng9812@126.com>
2019-12-19 20:50:41 +0800

    添加npm依赖

--- src/main.js
+++ src/main.js
@@ -7,11 +7,21 @@ import config from './config'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import './assets/material.css'
+import './http';
+import 'element-ui/lib/theme-chalk/index.css';
+import qs from 'qs'
+import MyComponent from './components/MyComponent'
 
 Vue.config.productionTip = false
 
+// 引用qs库
+Vue.prototype.$qs = qs;
+
 // 设置主题
 Vue.use(Vuetify, { theme: config.theme})
+
+// 封装组件
+Vue.use(MyComponent)
 
 /* eslint-disable no-new */
 new Vue({
